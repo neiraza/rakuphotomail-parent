bb3f25251d42e71df619b3064b5530cf68f03be8
tooru.oguri <tooru.oguri@fttx.co.jp>
2011-10-19 16:59:18 +0900

    メール作成画面レイアウト作成完了 refs #2319

---
+++ rakuphotomail/src/main/java/jp/co/fttx/rakuphotomail/activity/GallerySendingMail.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2011, UCOM Corporation and/or its affiliates. All rights reserved.
+ * UCOM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package jp.co.fttx.rakuphotomail.activity;
+
+import jp.co.fttx.rakuphotomail.R;
+import android.os.Bundle;
+//import android.widget.EditText;
+
+/**
+ * GallerySendingMail.
+ * @author tooru.oguri
+ *
+ */
+public class GallerySendingMail extends RakuPhotoActivity {
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.gallery_sending_mail);
+		
+//		EditText editText = (EditText) findViewById(R.id.gallery_mail_content);
+	}
+
+//	public void hoge(){
+//		
+//	}
+
+}
