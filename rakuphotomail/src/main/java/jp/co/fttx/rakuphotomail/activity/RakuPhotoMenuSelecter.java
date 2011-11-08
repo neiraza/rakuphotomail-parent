6e2bdf82035ab8bbf8b69ccf740c15366ef0abf1
tooru.oguri <tooru.oguri@fttx.co.jp>
2011-11-08 12:32:19 +0900

    モック画面作成＆AndroidSDKr12からr15にup。
    結果、maven plguinに影響が出たのでpomかきかえ。

--- rakuphotomail/src/main/java/jp/co/fttx/rakuphotomail/activity/RakuPhotoMenuSelecter.java
+++ rakuphotomail/src/main/java/jp/co/fttx/rakuphotomail/activity/RakuPhotoMenuSelecter.java
@@ -1,7 +1,5 @@
 package jp.co.fttx.rakuphotomail.activity;
 
-import jp.co.fttx.rakuphotomail.Account;
-import jp.co.fttx.rakuphotomail.Preferences;
 import jp.co.fttx.rakuphotomail.R;
 import jp.co.fttx.rakuphotomail.RakuPhotoMail;
 import android.app.Activity;
@@ -13,8 +11,6 @@ import android.widget.ImageView;
 
 public class RakuPhotoMenuSelecter extends Activity {
 	private ImageView mMail;
-	private ImageView mPhoto;
-	private Account account;
 	private Context mContext;
 
 	// private Button button;
@@ -24,38 +20,17 @@ public class RakuPhotoMenuSelecter extends Activity {
 		mContext = this;
 		super.onCreate(icicle);
 		setContentView(R.layout.rakuphoto_top);
-		Account[] accounts = Preferences.getPreferences(this).getAccounts();
-
-		// TODO account固定部分の修正が必要
-		if (null != accounts && accounts.length > 0) {
-			account = accounts[0];
-		} else {
-
-		}
 
 		mMail = (ImageView) findViewById(R.id.rakuphoto_mail);
-		mPhoto = (ImageView) findViewById(R.id.rakuphoto_photo);
 
 		mMail.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Log.v(RakuPhotoMail.LOG_TAG, "RakuPhotoMenuSelecter#onCreate:mail open!");
+				Log.v(RakuPhotoMail.LOG_TAG,
+						"RakuPhotoMenuSelecter#onCreate:mail open!");
 				DummyAccounts.listAccounts(mContext);
 				finish();
 			}
 		});
-		mPhoto.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Log.v(RakuPhotoMail.LOG_TAG, "RakuPhotoMenuSelecter#onCreate SlideShow Open!");
-				onSlideShow();
-			}
-		});
-	}
-	
-	private void onSlideShow() {
-		Account realAccount = account;
-		GallerySlideShow.actionHandleFolder(this, realAccount, realAccount.getInboxFolderName());
-		finish();
 	}
 }
