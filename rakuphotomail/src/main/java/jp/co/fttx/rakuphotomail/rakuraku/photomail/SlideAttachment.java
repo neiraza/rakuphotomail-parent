0fa14bd967e3286e391be1211783d4d391692bd4
tooru.oguri <tooru.oguri@fttx.co.jp>
2011-12-16 12:23:13 +0900

    いいよらくフォトいいよ

---
+++ rakuphotomail/src/main/java/jp/co/fttx/rakuphotomail/rakuraku/photomail/SlideAttachment.java
@@ -0,0 +1,11 @@
+package jp.co.fttx.rakuphotomail.rakuraku.photomail;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tooru.oguri
+ * Date: 11/12/15
+ * Time: 15:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class SlideAttachment {
+}
